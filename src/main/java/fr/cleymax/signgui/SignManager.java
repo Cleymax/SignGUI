8c25f7a2920f4b8ab8da8ca311ef86f626b65dc1
MedicOP <MedicOP@users.noreply.github.com>
2022-01-26 23:29:10 +0100

    1.18

--- src/main/java/fr/cleymax/signgui/SignManager.java
+++ src/main/java/fr/cleymax/signgui/SignManager.java
@@ -6,7 +6,7 @@ import io.netty.channel.ChannelPipeline;
 import net.minecraft.core.BlockPosition;
 import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
